df51ac79ff8f0e1292f3c9e51fe08b61c3f3bb0d
chris <christoph@roboterclub-freiburg.de>
2015-08-09 10:37:36 +0200

    Asuro functions separated

--- AsuroCompatibliltiy/AsuroCompatibliltiy.ino
+++ AsuroCompatibliltiy/AsuroCompatibliltiy.ino
@@ -1,111 +1,29 @@
 #include "arduAsuro.h"
 
-const int sensor = A1;
-const int buttonPin = 11;
-const int ledPin =  13;
-
-#define ENABLE_RIGHT 10
-#define MOTOR_RIGHT1  2
-#define MOTOR_RIGHT2  3
-
-#define ENABLE_LEFT  9
-#define MOTOR_LEFT1  A2
-#define MOTOR_LEFT2  A3
-
-int buttonState = 0;
-
-void MotorSpeed(uint8_t left, uint8_t right)
-{
-  analogWrite(ENABLE_RIGHT, left);
-  analogWrite(ENABLE_LEFT, right);
-}
-
-void MotorDir(uint8_t left, uint8_t right)
-{
-  //*************** left motor *****************
-  if (left == FWD)
-  {
-    digitalWrite(MOTOR_LEFT1, LOW);
-    digitalWrite(MOTOR_LEFT2, HIGH);
-  }
-  if (left == RWD)
-  {
-    digitalWrite(MOTOR_LEFT1, HIGH);
-    digitalWrite(MOTOR_LEFT2, LOW);
-  }
-  if (left == BREAK)
-  {
-    digitalWrite(MOTOR_LEFT1, LOW);
-    digitalWrite(MOTOR_LEFT2, LOW);
-    MotorSpeed(255, 255);
-  }
-  if (left == FREE)
-  {
-    MotorSpeed(0, 0);
-  }
-  //********** right motor ********************
-  if (right == FWD)
-  {
-
-        digitalWrite(MOTOR_RIGHT1, HIGH);
-    digitalWrite(MOTOR_RIGHT2, LOW);
-  }
-  if (right == RWD)
-  {
-    digitalWrite(MOTOR_RIGHT1, LOW);
-    digitalWrite(MOTOR_RIGHT2, HIGH);
-  }
-  if (right == BREAK)
-  {
-    digitalWrite(MOTOR_RIGHT1, LOW);
-    digitalWrite(MOTOR_RIGHT2, LOW);
-    MotorSpeed(255, 255);
-  }
-  if (right == FREE)
-  {
-    MotorSpeed(0, 0);
-  }
-
-}
-
+#define BUTTONPIN  11
+#define LEDPIN     13
 
 void setup()
 {
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(BUTTONPIN, INPUT_PULLUP);
 
-  pinMode(sensor, INPUT_PULLUP);
+  pinMode(LEDPIN, OUTPUT);
 
-  pinMode(ledPin, OUTPUT);
-
-  pinMode(ENABLE_RIGHT, OUTPUT);
-  pinMode(MOTOR_RIGHT1, OUTPUT);
-  pinMode(MOTOR_RIGHT2, OUTPUT);
-
-  pinMode(ENABLE_LEFT, OUTPUT);
-  pinMode(MOTOR_LEFT1, OUTPUT);
-  pinMode(MOTOR_LEFT2, OUTPUT);
-
-  // Motor rechts Vorwertsfahrt, dauerhaft
-  digitalWrite(MOTOR_RIGHT1, HIGH);
-  digitalWrite(MOTOR_RIGHT2, LOW);
-
-  // Motor links Vorwertsfahrt, dauerhaft
-  digitalWrite(MOTOR_LEFT1, LOW);
-  digitalWrite(MOTOR_LEFT2, HIGH);
+  Init();
 }
 
 void loop()
 {
   int n;
-  if (digitalRead(buttonPin))
+  if (digitalRead(BUTTONPIN))
   {
-    digitalWrite(ledPin, true);
+    digitalWrite(LEDPIN, true);
     delay(10);
-    digitalWrite(ledPin, false);
+    digitalWrite(LEDPIN, false);
     delay(500);
   } else
   {
-    digitalWrite(ledPin, false);
+    digitalWrite(LEDPIN, false);
 
     for (n = 0; n < 3; n++)
     {
@@ -132,7 +50,6 @@ void loop()
       MotorDir(FREE,FREE);
       
       delay(3000);
-
 
     }
   }
--- AsuroCompatibliltiy/arduAsuro.h
+++ AsuroCompatibliltiy/arduAsuro.h
@@ -1,10 +1,25 @@
+#include "Arduino.h"
 #include "stdint.h"
+
+
+
+#define ENABLE_RIGHT 10
+#define MOTOR_RIGHT1  2
+#define MOTOR_RIGHT2  3
+
+#define ENABLE_LEFT  9
+#define MOTOR_LEFT1  A2
+#define MOTOR_LEFT2  A3
 
 #define BREAK   0                     /*!< Motor bremsen */
 #define FREE    1                     /*!< Motor freilaufend */
 #define FWD     2                     /*!< Motor vorwaerts */
 #define RWD     3                     /*!< Motor rueckwaerts */
 
+void Init(void);
+void Msleep(uint32_t time_ms);
+void MotorSpeed(uint8_t left, uint8_t right);
+void MotorDir(uint8_t left, uint8_t right);
 
 //  MotorSpeed(150,150);
 //  MotorDir(FWD,FWD);
